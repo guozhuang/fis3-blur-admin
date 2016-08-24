78907c1717b00accaefc5f2a4fef4303c3a39171
liguwe <liguwe@qq.com>
2016-08-24 23:13:42 +0800

    fuck..

--- fis-conf.js
+++ fis-conf.js
@@ -13,15 +13,22 @@ fis.set('statics', '/statics'); //static目录
 /*************************目录规范*****************************/
 
 
-
-fis.match("src/**.js", {
-        release: '${statics}/$&'
+fis.match("**/*", {
+  release: '${statics}/$&'
 });
+
+
+fis.match("src/(*.html)", {
+        release: '$1'
+});
+
+
 
 //assets:字体，图片等目录，因为硬编码到js,scss文件中，先单独发布父级目录
-fis.match("src/assets/**", {
-        release: '/$&'
+fis.match("src/assets/(**)", {
+        release: '/assets/$1'
 });
+
 
 // 模拟数据目录
 fis.match("mock/**", {
@@ -52,19 +59,19 @@ fis.match('src/sass/**.scss', {
 
 
 //打包与css sprite基础配置
-fis.match('::packager', {
-    // npm install [-g] fis3-postpackager-loader
-    // 分析 __RESOURCE_MAP__ 结构，来解决资源加载问题
-    postpackager: fis.plugin('loader', {
-        resourceType: 'mod',
-        useInlineMap: true // 资源映射表内嵌
-    }),
-    packager: fis.plugin('map'),
-    spriter: fis.plugin('csssprites', {
-        layout: 'matrix',
-        margin: '15'
-    })
-})
+// fis.match('::packager', {
+//     // npm install [-g] fis3-postpackager-loader
+//     // 分析 __RESOURCE_MAP__ 结构，来解决资源加载问题
+//     postpackager: fis.plugin('loader', {
+//         resourceType: 'mod',
+//         useInlineMap: true // 资源映射表内嵌
+//     }),
+//     packager: fis.plugin('map'),
+//     spriter: fis.plugin('csssprites', {
+//         layout: 'matrix',
+//         margin: '15'
+//     })
+// })
 
 
 
